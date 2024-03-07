9a65ed7b805884d8bf80165ed0ec0e06aa56c8b0
Barath97 <barath.kj97@gmail.com>
2024-03-07 21:22:04 +0530

    Final Deployment is Ready

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin:[""],
+    origin:["https://elansol-mern.vercel.app"],
     methods:["POST","GET"],
     credentials:true
   }
